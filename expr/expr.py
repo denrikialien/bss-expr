e46322c4a0a30a41c6f9ff28c54ceb9b959724f5
denrikialien <hirameunagi@gmail.com>
2022-01-23 22:29:36 +0900

    foo

--- expr/expr.py
+++ expr/expr.py
@@ -1,5 +1,8 @@
 # %%
+from operator import mod
 import sys
+
+from black import json
 
 sys.path.append("../src")
 from experiment import experiment
@@ -7,6 +10,7 @@ from experiment import experiment
 
 # %%
 def run(cmd, fw, fh, ns, rm, no):
+    outdir = "./fw{}-fh{}-ns{}-rm{}-no{}".format(fw, fh, ns, rm, no)
     protocol = {
         "variables": {
             "field width": fw,
@@ -20,21 +24,25 @@ def run(cmd, fw, fh, ns, rm, no):
         "config": {
             "command": cmd,
             "outdir": "./fw{}-fh{}-ns{}-rm{}-no{}".format(fw, fh, ns, rm, no),
-            "trials": 2,
+            "trials": 100,
             "verbose": False,
         },
     }
 
     experiment(protocol)
 
+    with open("{}/protocol.json".format(outdir), mode="w") as f:
+        json.dump(protocol, f, indent=2)
+
 
 # %%
 field_width = list(range(200, 1000 + 1, 100))
 # field_height = list(range(200, 400 + 1, 100))
-field_height = 400
+field_height = [400]
 num_sensors = list(range(20, 100 + 1, 10))
 ratio_mobiles = list(range(20, 100 + 1, 10))
-num_obstacles = list(range(0, 100 + 1, 10))
+# num_obstacles = list(range(0, 100 + 1, 10))
+num_obstacles = [50]
 
 params = [
     (fw, fh, ns, rm, no)
