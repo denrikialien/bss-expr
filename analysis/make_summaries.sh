d2f704dc69ad6097475076651aad2150421f40f8
denrikialien <hirameunagi@gmail.com>
2022-01-24 19:34:46 +0900

    new exprs

--- analysis/make_summaries.sh
+++ analysis/make_summaries.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm *.csv
-python ../src/summary.py ../robustness-single/ single robustness-single.csv
-python ../src/summary.py ../robustness-multi-u/ multi-u robustness-multi-u.csv
-python ../src/summary.py ../basedist-single/ single basedist-single.csv
-python ../src/summary.py ../basedist-multi-u/ multi-u basedist-multi-u.csv
-python ../src/summary.py ../mobilerate-single/ single mobilerate-single.csv
-python ../src/summary.py ../mobilerate-multi-u/ multi-u mobilerate-multi-u.csv
+python ../src/summary.py ../output-robustness-single/ single robustness-single.csv
+python ../src/summary.py ../output-robustness-multi-u/ multi-u robustness-multi-u.csv
+python ../src/summary.py ../output-basedist-single/ single basedist-single.csv
+python ../src/summary.py ../output-basedist-multi-u/ multi-u basedist-multi-u.csv
+python ../src/summary.py ../output-mobilerate-single/ single mobilerate-single.csv
+python ../src/summary.py ../output-mobilerate-multi-u/ multi-u mobilerate-multi-u.csv
